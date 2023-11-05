83391c4ce6fc0831e48c9a14340990524638bb5a
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-05 16:41:09 +0300

    Add class Supervisor

---
+++ src/main/java/bks2101/kuraga/firstProject/models/Supervisor.java
@@ -0,0 +1,24 @@
+package bks2101.kuraga.firstProject.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
+public class Supervisor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String personal_data;
+    @OneToMany(mappedBy = "supervisor")
+    private Set<Curator> curators;
+}
