cb2819da783e271ab2ed936dc910a6b31cde17c6
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-05 16:41:26 +0300

    Update Class User

--- src/main/java/bks2101/kuraga/firstProject/models/User.java
+++ src/main/java/bks2101/kuraga/firstProject/models/User.java
@@ -1,5 +1,9 @@
 package bks2101.kuraga.firstProject.models;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -7,6 +11,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
 @Table(name = "users",
 uniqueConstraints = {
         @UniqueConstraint(columnNames = "username"),
@@ -19,56 +27,5 @@ public class User {
     private String username;
     private String password;
     private String email;
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "user_roles",
-    joinColumns = @JoinColumn(name = "user_id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id"))
-    @Fetch(FetchMode.JOIN)
-    private Set<Role> roles = new HashSet<>();
-    public Long getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public void setEmail(String email)
-    {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-    public User() {}
-    public User(String username, String email, String password) {
-        super();
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+    private ERole role;
 }
