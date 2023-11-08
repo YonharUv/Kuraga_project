e72c87136f6903e02b46a588849127426a09c719
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-08 20:40:25 +0300

    Delete

--- src/main/java/bks2101/kuraga/firstProject/repository/ERoleRepository.java
+++
@@ -1,9 +0,0 @@
-package bks2101.kuraga.firstProject.repository;
-
-import bks2101.kuraga.firstProject.models.ERole;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ERoleRepository extends JpaRepository<ERole, Long> {
-}
