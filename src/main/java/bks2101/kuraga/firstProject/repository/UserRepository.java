0c785d17c01e15193465c732988ebaeb6991c983
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-09 19:17:12 +0300

    Добавлены новые методы для репозитория User

--- src/main/java/bks2101/kuraga/firstProject/repository/UserRepository.java
+++ src/main/java/bks2101/kuraga/firstProject/repository/UserRepository.java
@@ -1,14 +1,19 @@
 package bks2101.kuraga.firstProject.repository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import bks2101.kuraga.firstProject.models.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-    Boolean existsByUsername(String username);
+
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+
     Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
 }
