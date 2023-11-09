f3d3abbe5a6bb3aac152f6b4ede7d69122866cdc
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-09 19:20:07 +0300

    Добавлен обработчик ошибок

---
+++ src/main/java/bks2101/kuraga/firstProject/controllers/ExceptionHandlerAdvice.java
@@ -0,0 +1,37 @@
+package bks2101.kuraga.firstProject.controllers;
+
+import bks2101.kuraga.firstProject.exceptions.UserAlreadyExistsException;
+import bks2101.kuraga.firstProject.exceptions.UserNotFoundByIdException;
+import bks2101.kuraga.firstProject.exceptions.UserNotFoundByUsernameException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandlerAdvice {
+    @ExceptionHandler(UserNotFoundByIdException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleUserNotFoundByIdException(UserNotFoundByIdException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+    @ExceptionHandler(UserNotFoundByUsernameException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleUserNotFoundByUsernameException(UserNotFoundByUsernameException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleUserAlreadyExists(UserAlreadyExistsException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+}
