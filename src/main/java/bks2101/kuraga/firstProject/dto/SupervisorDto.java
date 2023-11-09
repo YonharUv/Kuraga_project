7880b9cd00bc62190ad61a8b1620b87ad88654bb
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-09 19:17:24 +0300

    init

---
+++ src/main/java/bks2101/kuraga/firstProject/dto/SupervisorDto.java
@@ -0,0 +1,19 @@
+package bks2101.kuraga.firstProject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SupervisorDto {
+    private String first_name;
+    private String last_name;
+    private String personal_data;
+    private Set<Integer> curatorsId;
+}
