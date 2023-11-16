bc07e414778fe72612634f2f5cd136b257aec0be
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-16 10:47:05 +0300

    Renamed ApplicationUser and update Role

---
+++ src/main/java/bks2101/kuraga/firstProject/models/ApplicationUser.java
@@ -0,0 +1,29 @@
+package bks2101.kuraga.firstProject.models;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@Builder
+@Data
+@AllArgsConstructor
+@Table(name = "users",
+uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+public class ApplicationUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
