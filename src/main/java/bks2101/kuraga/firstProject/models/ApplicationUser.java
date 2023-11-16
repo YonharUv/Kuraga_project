750642210ffba4d48f2b9cc8006b6679a91649ef
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-16 20:42:05 +0300

    Change Role

--- src/main/java/bks2101/kuraga/firstProject/models/ApplicationUser.java
+++ src/main/java/bks2101/kuraga/firstProject/models/ApplicationUser.java
@@ -21,9 +21,6 @@ public class ApplicationUser {
     private String username;
     private String password;
     private String email;
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "users_roles",
-    joinColumns = @JoinColumn(name = "user_id"),
-    inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
