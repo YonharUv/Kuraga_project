47f87abaafc7cbba4f41d8fd6d340d118bc3d9ed
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-16 10:51:52 +0300

    Rename User to ApplicationUser

--- src/main/java/bks2101/kuraga/firstProject/repository/UserRepository.java
+++ src/main/java/bks2101/kuraga/firstProject/repository/UserRepository.java
@@ -1,19 +1,16 @@
 package bks2101.kuraga.firstProject.repository;
-import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Example;
+import bks2101.kuraga.firstProject.models.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import bks2101.kuraga.firstProject.models.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+public interface UserRepository extends JpaRepository<ApplicationUser, Long> {
+    Optional<ApplicationUser> findByUsername(String username);
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 
-    Optional<User> findByEmail(String email);
-    Optional<User> findById(Long id);
+    Optional<ApplicationUser> findByEmail(String email);
+    Optional<ApplicationUser> findById(Long id);
 }
