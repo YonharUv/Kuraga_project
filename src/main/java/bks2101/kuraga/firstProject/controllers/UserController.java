5e3ab60184aba465ffad273874797a1534200c22
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-16 20:45:07 +0300

    Update Controller for admin

--- src/main/java/bks2101/kuraga/firstProject/controllers/UserController.java
+++ src/main/java/bks2101/kuraga/firstProject/controllers/UserController.java
@@ -1,40 +1,26 @@
 package bks2101.kuraga.firstProject.controllers;
 
-import bks2101.kuraga.firstProject.exceptions.UserAlreadyExistsException;
 import bks2101.kuraga.firstProject.exceptions.UserNotFoundByIdException;
 import bks2101.kuraga.firstProject.exceptions.UserNotFoundByUsernameException;
-import bks2101.kuraga.firstProject.models.ApplicationUser;
-import bks2101.kuraga.firstProject.models.UserRole;
 import bks2101.kuraga.firstProject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 import static java.lang.String.format;
 
 @RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
     @GetMapping("/users")
     ResponseEntity getAllUsers() {
         return ResponseEntity.ok(userRepository.findAll());
     }
-//    @PostMapping("/users")
-//    public ResponseEntity createUser(@RequestBody ApplicationUser applicationUser) throws UserAlreadyExistsException {
-//        if (userRepository.existsByUsername(applicationUser.getUsername()) || userRepository.existsByEmail(applicationUser.getEmail())) {
-//            throw new UserAlreadyExistsException(applicationUser.getEmail(), applicationUser.getUsername());
-//        }
-//        try {
-//            applicationUser.setRoles(Set<Role>);
-//            userRepository.save(applicationUser);
-//            return ResponseEntity.ok("Пользователь успешно сохранен");
-//        } catch (Exception e) {
-//            return ResponseEntity.badRequest().body("Произошла ошибка");
-//        }
-//    }
     @GetMapping("/users/{id}")
     public ResponseEntity getUserById(@PathVariable Long id) throws UserNotFoundByIdException {
         if (!userRepository.existsById(id)) {
