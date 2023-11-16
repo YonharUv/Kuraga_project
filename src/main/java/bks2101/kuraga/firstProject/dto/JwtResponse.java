0ce4065d9331c53ff2bc45d647c77adffb91e7ef
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-16 10:48:19 +0300

    Add JwtResponse

---
+++ src/main/java/bks2101/kuraga/firstProject/dto/JwtResponse.java
@@ -0,0 +1,10 @@
+package bks2101.kuraga.firstProject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+}
