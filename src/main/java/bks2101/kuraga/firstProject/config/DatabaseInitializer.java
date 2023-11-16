a7e24d1b5ed4f69f710ba1e956c93222f8eb2e33
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-16 20:42:46 +0300

    Initializer db. Add admin if not exists

---
+++ src/main/java/bks2101/kuraga/firstProject/config/DatabaseInitializer.java
@@ -0,0 +1,34 @@
+package bks2101.kuraga.firstProject.config;
+
+import bks2101.kuraga.firstProject.models.ApplicationUser;
+import bks2101.kuraga.firstProject.models.Role;
+import bks2101.kuraga.firstProject.repository.UserRepository;
+import bks2101.kuraga.firstProject.service.UserDetailsServiceImpl;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseInitializer {
+    private final UserDetailsServiceImpl userService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Transactional
+    @Bean
+    public void initializeDatabase() {
+        String adminUsername = "admin";
+        if (!userService.existsByUsername(adminUsername)) {
+            ApplicationUser admin = new ApplicationUser();
+            admin.setUsername(adminUsername);
+            admin.setEmail("admin@example.com");
+            admin.setPassword(passwordEncoder.encode("adminPassword"));
+            admin.setRole(Role.ADMIN);
+            userRepository.save(admin);
+        }
+    }
+}
