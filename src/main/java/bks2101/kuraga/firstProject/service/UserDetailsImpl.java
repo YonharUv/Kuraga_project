7baa08390d3c73edc29c394573849104ff698692
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-16 10:51:19 +0300

    Update

---
+++ src/main/java/bks2101/kuraga/firstProject/service/UserDetailsImpl.java
@@ -0,0 +1,70 @@
+package bks2101.kuraga.firstProject.service;
+
+
+import bks2101.kuraga.firstProject.models.ApplicationUser;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private final String username;
+    private final String password;
+    private final List<GrantedAuthority> authorities;
+    private final boolean isActive;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+    public static UserDetails fromUser(ApplicationUser user) {
+        var authorities = user.getRoles().stream()
+                .map(role -> role.getName().grantedAuthority())
+                .flatMap(Set::stream)
+                .toList();
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                authorities);
+    }
+}
