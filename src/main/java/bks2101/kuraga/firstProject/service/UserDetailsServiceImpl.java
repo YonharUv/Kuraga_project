e840b9dce83758fc7d66a13d2c727981f9476467
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-17 14:56:50 +0300

    Update add new funcs

--- src/main/java/bks2101/kuraga/firstProject/service/UserDetailsServiceImpl.java
+++ src/main/java/bks2101/kuraga/firstProject/service/UserDetailsServiceImpl.java
@@ -1,17 +1,20 @@
 package bks2101.kuraga.firstProject.service;
 
 import bks2101.kuraga.firstProject.dto.RegistrationUserDto;
+import bks2101.kuraga.firstProject.exceptions.NotFoundByIdException;
+import bks2101.kuraga.firstProject.exceptions.UserNotFoundByUsernameException;
 import bks2101.kuraga.firstProject.models.ApplicationUser;
 import bks2101.kuraga.firstProject.models.Role;
 import bks2101.kuraga.firstProject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -32,7 +35,41 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         user.setRole(Role.USER);
         return userRepository.save(user);
     }
+    public ResponseEntity getUserByUsername(String username) throws UserNotFoundByUsernameException {
+        if (!userRepository.existsByUsername(username)){
+            throw new UserNotFoundByUsernameException(username);
+        }
+        return ResponseEntity.ok(userRepository.findByUsername(username));
+    }
     public boolean existsByUsername(String username) {
         return userRepository.existsByUsername(username);
+    }
+    public ResponseEntity getUserByID(Long id) throws NotFoundByIdException {
+        if (!userRepository.existsById(id)) {
+            throw new NotFoundByIdException("Пользователя",id);
+        }
+        return ResponseEntity.ok(userRepository.findById(id));
+    }
+    public ResponseEntity getAllUser() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+    public ResponseEntity deleteUserByID(long id) throws NotFoundByIdException {
+        if (!userRepository.existsById(id)) {
+            throw new NotFoundByIdException("Пользователя",id);
+        }
+        userRepository.deleteById(id);
+        return ResponseEntity.ok(format("Пользователь с id = %d успешно удален", id));
+    }
+    public ResponseEntity exchangeUser(ApplicationUser newUser, long id) throws NotFoundByIdException {
+        if (!userRepository.existsById(id)) {
+            throw new NotFoundByIdException("Пользователя", id);
+        }
+        return ResponseEntity.ok(userRepository.findById(id).map(applicationUser -> {
+            applicationUser.setUsername(newUser.getUsername());
+            applicationUser.setEmail(newUser.getEmail());
+            applicationUser.setPassword(newUser.getPassword());
+            applicationUser.setRole(newUser.getRole());
+            return userRepository.save(applicationUser);
+        }));
     }
 }
