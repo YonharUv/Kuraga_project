c3775634b079ac1da986918fd055dca9d179f78d
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-17 14:52:28 +0300

    Create

---
+++ src/main/java/bks2101/kuraga/firstProject/dto/GroupDto.java
@@ -0,0 +1,13 @@
+package bks2101.kuraga.firstProject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupDto {
+    private String name;
+}
