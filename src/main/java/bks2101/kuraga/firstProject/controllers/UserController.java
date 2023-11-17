c1757292b717191a3d7d11bdeecca80198e5f436
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-17 14:56:27 +0300

    Update with UserService

--- src/main/java/bks2101/kuraga/firstProject/controllers/UserController.java
+++ src/main/java/bks2101/kuraga/firstProject/controllers/UserController.java
@@ -1,12 +1,12 @@
 package bks2101.kuraga.firstProject.controllers;
 
-import bks2101.kuraga.firstProject.exceptions.UserNotFoundByIdException;
+import bks2101.kuraga.firstProject.exceptions.NotFoundByIdException;
 import bks2101.kuraga.firstProject.exceptions.UserNotFoundByUsernameException;
-import bks2101.kuraga.firstProject.repository.UserRepository;
+import bks2101.kuraga.firstProject.models.ApplicationUser;
+import bks2101.kuraga.firstProject.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import static java.lang.String.format;
@@ -16,44 +16,25 @@ import static java.lang.String.format;
 @RequiredArgsConstructor
 public class UserController {
     @Autowired
-    private final UserRepository userRepository;
+    private final UserDetailsServiceImpl userService;
     @GetMapping("/users")
     ResponseEntity getAllUsers() {
-        return ResponseEntity.ok(userRepository.findAll());
+        return userService.getAllUser();
     }
     @GetMapping("/users/{id}")
-    public ResponseEntity getUserById(@PathVariable Long id) throws UserNotFoundByIdException {
-        if (!userRepository.existsById(id)) {
-            throw new UserNotFoundByIdException(id);
-        }
-        return ResponseEntity.ok(userRepository.findById(id));
+    public ResponseEntity getUserById(@PathVariable Long id) throws NotFoundByIdException {
+        return userService.getUserByID(id);
     }
     @GetMapping("/users/find/{username}")
     public ResponseEntity getUserByUsername(@PathVariable String username) throws UserNotFoundByUsernameException {
-        if (!userRepository.existsByUsername(username)){
-            throw new UserNotFoundByUsernameException(username);
-        }
-        return ResponseEntity.ok(userRepository.findByUsername(username));
+        return userService.getUserByUsername(username);
     }
-//    @PutMapping("/users/{id}")
-//    public ResponseEntity<Optional<ApplicationUser>> exchangeUser(@RequestBody ApplicationUser newApplicationUser, @PathVariable Long id) throws UserNotFoundByIdException {
-//        if (!userRepository.existsById(id)) {
-//            throw new UserNotFoundByIdException(id);
-//        }
-//        return ResponseEntity.ok(userRepository.findById(id).map(applicationUser -> {
-//            applicationUser.setUsername(newApplicationUser.getUsername());
-//            applicationUser.setEmail(newApplicationUser.getEmail());
-//            applicationUser.setPassword(newApplicationUser.getPassword());
-//            applicationUser.setRole(UserRole.USER);
-//            return userRepository.save(applicationUser);
-//        }));
-//    }
+    @PutMapping("/users/{id}")
+    public ResponseEntity exchangeUser(@RequestBody ApplicationUser newApplicationUser, @PathVariable Long id) throws NotFoundByIdException {
+        return userService.exchangeUser(newApplicationUser, id);
+    }
     @DeleteMapping("/users/{id}")
-    public ResponseEntity deleteUser(@PathVariable Long id) throws UserNotFoundByIdException {
-        if (!userRepository.existsById(id)) {
-            throw new UserNotFoundByIdException(id);
-        }
-        userRepository.deleteById(id);
-        return ResponseEntity.ok(format("Пользователь с id = %d успешно удален", id));
+    public ResponseEntity deleteUser(@PathVariable Long id) throws NotFoundByIdException {
+        return userService.deleteUserByID(id);
     }
 }
