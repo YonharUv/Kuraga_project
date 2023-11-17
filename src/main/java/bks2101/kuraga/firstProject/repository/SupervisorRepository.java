e3198297df209e0c84d81c4800938df8f077eab1
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-17 14:55:40 +0300

    Add func findByUsername

--- src/main/java/bks2101/kuraga/firstProject/repository/SupervisorRepository.java
+++ src/main/java/bks2101/kuraga/firstProject/repository/SupervisorRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SupervisorRepository extends JpaRepository<Supervisor, Long> {
+    public Supervisor findByUsername(String username);
 }
