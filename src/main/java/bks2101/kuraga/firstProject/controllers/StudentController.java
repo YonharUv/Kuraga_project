21e54fc08ac3b3ae6304bf9751dfd994e87beb72
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-11-17 14:54:20 +0300

    Update update with StudentService

--- src/main/java/bks2101/kuraga/firstProject/controllers/StudentController.java
+++ src/main/java/bks2101/kuraga/firstProject/controllers/StudentController.java
@@ -1,26 +1,34 @@
 package bks2101.kuraga.firstProject.controllers;
 
+import bks2101.kuraga.firstProject.dto.StudentDto;
+import bks2101.kuraga.firstProject.exceptions.NotFoundByIdException;
+import bks2101.kuraga.firstProject.exceptions.UserAlreadyExistsException;
 import bks2101.kuraga.firstProject.models.Student;
 import bks2101.kuraga.firstProject.repository.StudentRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
+import bks2101.kuraga.firstProject.service.StudentService;
+import bks2101.kuraga.firstProject.utils.JwtTokenUtils;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
 @RestController
+@RequiredArgsConstructor
 public class StudentController {
-    private final StudentRepository studentRepository;
-    public StudentController(StudentRepository repository) {
-        this.studentRepository = repository;
-    }
+    private final StudentService studentService;
+
     @GetMapping("/students")
-    List<Student> all() {
-        return studentRepository.findAll();
+    ResponseEntity getAllStudents() {
+        return ResponseEntity.ok(studentService.getAllStudents());
     }
     @PostMapping("/students")
-    Student newStudent(@RequestBody Student student) {
-        return studentRepository.save(student);
+    ResponseEntity newStudent(@RequestHeader("Authorization") String authHeader, @RequestBody StudentDto student) throws UserAlreadyExistsException {
+        return studentService.createStudent(authHeader, student);
+    }
+    @PostMapping("/students/{username}/group")
+    ResponseEntity setGroupStudent(@PathVariable String username) throws UserAlreadyExistsException {
+
+        return ResponseEntity.ok("");
     }
 }
