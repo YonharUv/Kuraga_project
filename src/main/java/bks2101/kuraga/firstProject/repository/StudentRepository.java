863231cfe7e339844ae4741f5a79d6fb6269ac5e
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-12-05 18:01:27 +0300

    Update add funcs for repository

--- src/main/java/bks2101/kuraga/firstProject/repository/StudentRepository.java
+++ src/main/java/bks2101/kuraga/firstProject/repository/StudentRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    boolean existsByUsername(String username);
-    Student findByUsername(String username);
+    boolean existsByEmail(String username);
+    Student findByEmail(String username);
 }
