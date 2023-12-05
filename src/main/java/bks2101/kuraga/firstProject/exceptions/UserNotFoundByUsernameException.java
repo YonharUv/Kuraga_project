42aaedede70954e124552bc23cfddae04421856e
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-12-05 17:57:09 +0300

    Update

--- src/main/java/bks2101/kuraga/firstProject/exceptions/UserNotFoundByUsernameException.java
+++ src/main/java/bks2101/kuraga/firstProject/exceptions/UserNotFoundByUsernameException.java
@@ -1,8 +1,10 @@
 package bks2101.kuraga.firstProject.exceptions;
 
+import static java.lang.String.format;
+
 public class UserNotFoundByUsernameException extends Exception{
-    public UserNotFoundByUsernameException(String username) {
-        super("Пользователя " + username + " не существует");
+    public UserNotFoundByUsernameException(String name, String email) {
+        super(format("%s %s не существует", name, email));
     }
     @Override
     public synchronized Throwable fillInStackTrace() {
