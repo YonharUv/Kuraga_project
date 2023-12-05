db105cde9a2e4a2c98fb9a6568b195851bd6ca85
Ivan Kuznetsov <nepisatmne80@gmail.com>
2023-12-05 18:00:40 +0300

    Update: add new fields

--- src/main/java/bks2101/kuraga/firstProject/dto/GroupDto.java
+++ src/main/java/bks2101/kuraga/firstProject/dto/GroupDto.java
@@ -1,13 +1,21 @@
 package bks2101.kuraga.firstProject.dto;
 
+import bks2101.kuraga.firstProject.models.Student;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class GroupDto {
     private String name;
+    private String faculty_name;
+    private String curator_email;
+    private Set<StudentDto> students;
+    private Set<MeetingDto> meetings;
 }
