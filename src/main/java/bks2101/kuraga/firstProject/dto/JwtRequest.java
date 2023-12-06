c9f530cb769e82363880ffc4bf76c421b18606e5
nikita <nikita@example.com>
2023-12-06 14:14:27 +0300

    clear before clone dev

--- src/main/java/bks2101/kuraga/firstProject/dto/JwtRequest.java
+++
@@ -1,9 +0,0 @@
-package bks2101.kuraga.firstProject.dto;
-
-import lombok.Data;
-
-@Data
-public class JwtRequest {
-    private String username;
-    private String password;
-}
--- src/main/java/bks2101/kuraga/firstProject/dto/RequestAddCurators.java
+++
@@ -1,9 +0,0 @@
-package bks2101.kuraga.firstProject.dto;
-
-import lombok.Data;
-
-import java.util.Set;
-@Data
-public class RequestAddCurators {
-    private Set<String> curators_email;
-}
